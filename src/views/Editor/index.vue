042ec3290ecb2739cacf1740322e10f0a518d4a3
long020806 <cstslswust@163.com>
2023-11-01 11:37:45 +0800

    删除默认css

--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="flex items-center">
+        <h1 class="text-center text-lg text-blue-500">Hello World</h1>
         <header>1</header>
         <main>2</main>
         <footer>3</footer>
