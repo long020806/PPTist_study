15187d60cfa745d5e8140d617e95c5eacba0ea2a
long020806 <cstslswust@163.com>
2023-11-28 22:41:47 +0800

    tailwindcss布局

---
+++ src/tailwindcss/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center">
+        <h1 class="text-center text-lg text-blue-500">Hello World</h1>
+        <header>1</header>
+        <main>2</main>
+        <footer>3</footer>
+    </div>
+</template>
+<script lang='ts' setup>
+
+
+</script>
+<style scoped>
+
+</style>
--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -1,9 +1,25 @@
 <template>
-    <div class="flex items-center">
-        <h1 class="text-center text-lg text-blue-500">Hello World</h1>
-        <header>1</header>
-        <main>2</main>
-        <footer>3</footer>
+    <div class="flex flex-col bg-white h-screen">
+        <div class="h-[40px] border border-slate-200 flex flex-row justify-between">
+            <div class="h-full flex items-center pl-5">
+                <div class="p-2 text-base font-sans font-light hover:bg-slate-200 flex items-center h-[80%]">
+                    <svg width="1em" height="1em" viewBox="0 0 48 48" fill="none"><path d="M7.94971 11.9497H39.9497" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7.94971 23.9497H39.9497" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7.94971 35.9497H39.9497" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                </div>
+                <div class="p-2 text-xs font-sans hover:bg-slate-200 flex  items-center h-full h-[80%]">
+                    <span>未命名演示文稿</span>
+                </div>
+            </div>
+            <div  class="h-full flex">
+                <div class="h-full">3</div>
+                <div class="h-full">4</div>
+                <div class="h-full">5</div>
+            </div>
+        </div>
+        <div class="flex flex-row bg-white h-full">
+            <div class="basis-40 border-r border-slate-200">1</div>
+            <div class="grow bg-slate-50 ">2</div>
+            <div class="basis-64 bg-white border-l border-slate-200">3</div>
+        </div>
     </div>
 </template>
 <script lang='ts' setup>
@@ -11,5 +27,4 @@
 
 </script>
 <style scoped>
-
 </style>
