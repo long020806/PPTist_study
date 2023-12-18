5af0e6ed4706f3fa20a325f7e18e1c76b9304f88
long020806 <cstslswust@163.com>
2023-12-18 17:43:58 +0800

    useLoading 更新

--- src/views/Editor/index.vue
+++ src/views/Editor/index.vue
@@ -2,28 +2,37 @@
   <div class=" ">
     <main class="p-5">
       {{ resultRef }}
-      <ul>
-        <li v-for="item in userList" :key="item.id">
-          <div>{{ item.id }}-{{ item.username }}-{{ item.created_at }}</div>
-        </li>
-      </ul>
+      <template v-if="isLoading">
+        加载中
+      </template>
+      <template v-else>
+        <ul >
+          <li v-for="item in userList" :key="item.id" >
+            <div>{{ item.id }}-{{ item.username }}-{{ item.created_at }}</div>
+          </li>
+        </ul>
+      </template>
+
 
     </main>
   </div>
 </template>
 <script lang="ts" setup>
 import {hello, list, User} from "@/api/hello/index.js";
+import { useLoading } from "@/utils/useLoading";
 import { ref } from "vue";
+const {isLoading,loadingWrapper} = useLoading();
 let resultRef = ref<string>("")
 let userList = ref<Array<User>>([]);
 hello().then(res=>{
   console.log(res)
   resultRef.value = res;
 });
-list({page:10,size:100}).then(res=>{
-  console.log(res,"userList");
-  userList.value = res.data.records;
-})
+
+loadingWrapper(list({page:90000,size:100}).then(res=>{
+  userList.value = res.data.records
+}));
+
 </script>
 <style scoped>
 </style>
